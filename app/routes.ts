79755a3ab5ed1dbe73f040ac9c40566fe0798ddd
Martin <martinberg9@proton.me>
2024-12-03 08:51:54 +0100

    general init cleanup, change home to index, export db connection

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("./Home.tsx"),
+  index("./Index.tsx"),
   route("about", "./components/About.tsx"),
 ] satisfies RouteConfig;
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,4 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-// console.log(process.env.DATABASE_URL);
-const db = drizzle(process.env.DATABASE_URL!);
-// console.log(db);
+
+export const db = drizzle(process.env.DATABASE_URL!);
